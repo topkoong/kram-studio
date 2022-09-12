cf43db22f70213da5fc7889376dd2ed04add44c8
toppy <theerut@shoplive.dev>
2022-09-12 19:32:38 +0700

    updated decoration color

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -30,42 +30,42 @@
 		<ul class="flex justify-center uppercase my-2">
 			<li
 				class="{$page.url.pathname === base
-					? 'underline underline-offset-8 decoration-4 decoration-bright-green'
+					? 'underline underline-offset-8 decoration-4 decoration-[#71CC00]'
 					: ''} text-sm md:text-lg mx-2"
 			>
 				<a href={base}>Home</a>
 			</li>
 			<li
 				class="{$page.url.pathname === `${base}/services`
-					? 'underline underline-offset-8 decoration-4 decoration-bright-green'
+					? 'underline underline-offset-8 decoration-4 decoration-[#71CC00]'
 					: ''} text-sm md:text-lg mx-2"
 			>
 				<a href="{base}/services">Services</a>
 			</li>
 			<li
 				class="{$page.url.pathname === `${base}/studios`
-					? 'underline underline-offset-8 decoration-4 decoration-bright-green'
+					? 'underline underline-offset-8 decoration-4 decoration-[#71CC00]'
 					: ''} text-sm md:text-lg mx-2"
 			>
 				<a href="{base}/studios">Studios</a>
 			</li>
 			<li
 				class="{$page.url.pathname === `${base}/about-us`
-					? 'underline underline-offset-8 decoration-4 decoration-bright-green'
+					? 'underline underline-offset-8 decoration-4 decoration-[#71CC00]'
 					: ''} hidden md:block text-sm md:text-lg mx-2"
 			>
 				<a href="{base}/about-us">About Us</a>
 			</li>
 			<li
 				class="{$page.url.pathname === `${base}/faqs`
-					? 'underline underline-offset-8 decoration-4 decoration-bright-green'
+					? 'underline underline-offset-8 decoration-4 decoration-[#71CC00]'
 					: ''} hidden md:block text-sm md:text-lg mx-2"
 			>
 				<a href="{base}/faqs">Faqs</a>
 			</li>
 			<li
 				class="{$page.url.pathname === `${base}/contact-us`
-					? 'underline underline-offset-8 decoration-4 decoration-bright-green'
+					? 'underline underline-offset-8 decoration-4 decoration-[#71CC00]'
 					: ''} text-sm md:text-lg mx-2"
 			>
 				<a href="{base}/contact-us">Contact Us</a>
